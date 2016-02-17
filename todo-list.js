c1834dd2f7fceeb2778727a0f25f71bccd2af6d3
Vinicius Minatogawa <vinicius@cyclum.in>
2016-02-17 09:59:03 -0200

    chapter 2 lessons included

--- todo-list.js
+++ todo-list.js
@@ -1,19 +1,37 @@
+Todos = new Mongo.Collection('todos');
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.todos.helpers({
+    'todo':function(){
+      return Todos.find({}, {sort: {createdAt:-1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.addTodo.events({
+    'submit form':function(event) {
+      event.preventDefault();
+      var todoName = $('[name="todoName"]').val();
+      Todos.insert({
+        name: todoName,
+        completed: false,
+        createdAt: new Date(),
+      });
+      $('[name="todoName"]').val("");
     }
   });
+
+  Template.todoItem.events({
+    'click .delete-todo':function(event){
+      event.preventDefault();
+      var documentId = this._id;
+      var confirm = window.confirm("Delete this taks?");
+        if(confirm){
+          Todos.remove({_id: documentId});
+        }
+    }
+  })
 }
 
 if (Meteor.isServer) {
